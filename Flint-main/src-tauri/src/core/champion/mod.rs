a917cd0e8adc659b814926f26bca7481f383bb0a
FrogDivine <lustigleandro@gmail.com>
2026-02-23 11:33:17 +0100

    Delete Flint-main directory

--- Flint-main/src-tauri/src/core/champion/mod.rs
+++
@@ -1,4 +0,0 @@
-// Champion discovery module exports
-pub mod discovery;
-
-pub use discovery::{discover_champions, get_champion_skins, ChampionInfo, SkinInfo};
--- Flint-main/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-// Declare Vite worker import syntax
-declare module '*?worker' {
-    const workerConstructor: {
-        new (): Worker;
-    };
-    export default workerConstructor;
-}
